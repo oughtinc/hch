0ccbf4d001b11d701a5686b562079471210b08f2
Andreas Stuhlmueller <andreas@stuhlmueller.org>
2017-08-07 15:29:39 -0700

    Use Fermi question as starting point

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { HCH } from "./hch";
 import { parseMessage } from "./parser";
 
 const hch = new HCH(human, 10e6);
-const startMessage = parseMessage("What is the reverse of (1 (2 (3)))?");
+const startMessage = parseMessage(
+  "How many (ping pong balls) fit into a (Boeing 747)"
+);
 const result = hch.act(startMessage);
 console.log(result.action.toString());
